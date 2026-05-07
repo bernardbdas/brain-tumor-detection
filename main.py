d1da76303de1481252b2b4d414f244e077c0c66e
bernardbdas <bernarddas1@gmail.com>
2026-05-07 15:03:16 +0530

    feat: implement Flask app for brain tumor detection with model loading and prediction functionality

--- main.py
+++ main.py
@@ -1,6 +1,77 @@
-def main():
-    print("Hello from brain-tumor-detection!")
+from flask import Flask, render_template, request, send_from_directory
+from tensorflow.keras.models import load_model
+from keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+import os
+
+app = Flask(__name__)
+
+models_dir = "models"
+available_models = []
+
+for filename in os.listdir(models_dir):
+    if filename.endswith((".h5", ".keras")):
+        model_path = os.path.join(models_dir, filename)
+        available_models.append(model_path)
+idx = 4
+print(f"Loading model at index {idx}: {available_models[idx]}")
+model = load_model(available_models[idx])
+
+class_labels = ["pituitary", "glioma", "notumor", "meningioma"]
+
+UPLOAD_FOLDER = "./uploads"
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
+def predict_tumor(image_path):
+    IMAGE_SIZE = 128
+    img = load_img(image_path, target_size=(IMAGE_SIZE, IMAGE_SIZE))
+    img_array = img_to_array(img) / 255.0  # Normalize pixel values
+    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+
+    predictions = model.predict(img_array)
+    predicted_class_index = np.argmax(predictions, axis=1)[0]
+    confidence_score = np.max(predictions, axis=1)[0]
+
+    if class_labels[predicted_class_index] == "notumor":
+        return "No Tumor", confidence_score
+    else:
+        return f"Tumor: {class_labels[predicted_class_index]}", confidence_score
+
+
+# Route for the main page (index.html)
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        # Handle file upload
+        file = request.files["file"]
+        if file:
+            # Save the file
+            file_location = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+            file.save(file_location)
+
+            # Predict the tumor
+            result, confidence = predict_tumor(file_location)
+
+            # Return result along with image path for display
+            return render_template(
+                "index.html",
+                result=result,
+                confidence=f"{confidence*100:.2f}%",
+                file_path=f"/uploads/{file.filename}",
+            )
+
+    return render_template("index.html", result=None)
+
+
+# Route to serve uploaded files
+@app.route("/uploads/<filename>")
+def get_uploaded_file(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
